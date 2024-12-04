81fa4020e976f4ec8be8faa8e6694cddb4b1f95c
xue0211 <zhengtai1123.qq.com>
2024-12-04 10:22:49 +0800

    Header组件初步成型

--- cnode-project/src/components/Header.vue
+++ cnode-project/src/components/Header.vue
@@ -1,11 +1,47 @@
 <template>
-    
+    <div class="header">
+        <img src="../assets/cnodejs_light.svg" alt="">
+        <ul>
+            <li>首页</li>
+            <li><a href="#">新手入门</a></li>
+            <li><a href="#">API</a></li>
+            <li><a href="#">关于</a></li>
+            <li>注册</li>
+            <li>登录</li>
+        </ul>
+    </div>
 </template>
 
 <script>
-
+export default{
+    name: 'Header',
+}
 </script>
 
-<style>
-
+<style scoped>
+.header{    
+  background-color: #444;
+  height:50px;
+}
+img{
+  max-width: 120px;
+  margin-left: 50px;
+  margin-top: 10px;
+}
+ul{
+  list-style: none;
+  float: right;
+  margin-right: 50px;
+}
+li{
+  display: inline-block;
+  line-height: 50px;
+  padding: 0 10px
+}
+a{
+  text-decoration: none;
+  color: #ccc;
+  font-size: 13px;
+  text-shadow: none;
+}
 </style>
